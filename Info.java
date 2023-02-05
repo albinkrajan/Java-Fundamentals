12a8095e88e5368e07ee6812e207c7d8e3a7775d
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2023-02-05 22:03:35 +0530

    Update Info.java

--- Info.java
+++ Info.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 class Info{
 	public static void main(String[] args){
 		String wrd;
+		char wrdA;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a String:");
 		wrd = sc.nextLine();
+		System.out.println("Enter a Character:");
+		wrdA = sc.next().charAt(0);
 		System.out.println("Information:"+wrd);
+		System.out.println("Character:"+wrdA);
 	}
 }
