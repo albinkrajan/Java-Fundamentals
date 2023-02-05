753637c54fbf1c48ed561081ba85de522dd7ea3d
ALBIN K RAJAN <90998736+albinkrajan@users.noreply.github.com>
2023-02-05 12:37:32 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Info{
+	public static void main(String[] args){
+		String wrd;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String:");
+		wrd = sc.nextLine();
+		System.out.println("Information:"+wrd);
+	}
+}
